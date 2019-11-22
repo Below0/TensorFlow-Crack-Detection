2dba7270786694547435e0ebf8574a86b94ca25c
Below0 <53803114+Below0@users.noreply.github.com>
2019-11-23 06:03:25 +0900

    Initial commit
    ICT COC Hackerthon

---
+++ Object_detection_video.py
@@ -0,0 +1,93 @@
+######## Video Object Detection Using Tensorflow-trained Classifier #########
+#
+# Author: Evan Juras
+# Date: 1/16/18
+# Description: 
+# This program uses a TensorFlow-trained classifier to perform object detection.
+# It loads the classifier and uses it to perform object detection on a video.
+# It draws boxes, scores, and labels around the objects of interest in each
+# frame of the video.
+
+## Some of the code is copied from Google's example at
+## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+
+## and some is copied from Dat Tran's example at
+## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
+
+## but I changed it to make it more understandable to me.
+
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+MODEL_NAME = 'inference_grap'
+VIDEO_NAME = input('Video name : ')
+
+CWD_PATH = os.getcwd()
+
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
+PATH_TO_VIDEO = os.path.join(CWD_PATH,VIDEO_NAME)
+
+NUM_CLASSES = 11
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+video = cv2.VideoCapture(PATH_TO_VIDEO)
+
+while(video.isOpened()):
+
+    ret, frame = video.read()
+    frame_expanded = np.expand_dims(frame, axis=0)
+
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+   
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.60)
+
+    cv2.imshow('Object detector', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
